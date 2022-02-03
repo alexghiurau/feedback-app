0a7f306f07ab544064fa856546b8c9c0fcf822ea
Alex Ghiurau <20340987+alexghiurau@users.noreply.github.com>
2022-02-03 23:42:57 +0000

    testing on new windows install

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header text='Hello famalam' />
+      <Header text='Hello there!' />
       <div className='container'>
         <h1>My app</h1>
       </div>
