6ccdccb04fb5d8d7fe985125b6438f72c99fa441
Alex Ghiurau <20340987+alexghiurau@users.noreply.github.com>
2022-02-06 19:53:20 +0000

    changes

--- src/components/FeedbackItem.tsx
+++ src/components/FeedbackItem.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import Card from './shared/Card';
 
 function FeedbackItem({ item }: FeedbackItemProps) {
   return (
-    <div className='card'>
+    <Card>
       <div className='num-display'>{item.rating}</div>
       <div className='text-display'>{item.text}</div>
-    </div>
+    </Card>
   );
 }
 
